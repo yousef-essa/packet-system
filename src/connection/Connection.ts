e6d694eb3eed7d69ba1c86acc6216a0083e16078
yousef-essa <yousef.essa.p@gmail.com>
2022-06-27 02:09:47 +0200

    Add onClose method to Connection
    The method has been added to give implementers the ability to dispose of
    the respective user data whenever it gets invoked.

--- src/connection/Connection.ts
+++ src/connection/Connection.ts
@@ -2,7 +2,11 @@ export default abstract class Connection {
     abstract id(): string
 
     abstract send(message: string): void
+
     abstract close(): void
+
+    onClose(): void {
+    }
 
     abstract isClosed(): boolean
 }
