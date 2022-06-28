7fba7ff0354ecbd1a69fd632e935f171e37e7242
yousef-essa <yousef.essa.p@gmail.com>
2022-06-28 21:02:10 +0200

    Add helpful utility methods
    These methods will make retrieving the packet type much easier.

--- README.md
+++ README.md
@@ -1,10 +1,12 @@
 # packet-system
+![Npm version of packet-system](https://img.shields.io/npm/v/packet-system)
 This is a simple, and easy to use packet system that I created to experiment around with JS networking.
 
 **Warning**: Please do note that this package is on experimental phase. I do not recommend this to be used in production in any way shape of form.
 
-The samples can be found [here](https://github.com/yousef-essa/packet-system-sample).  
-The documentation can be found [here](https://github.com/yousef-essa/packet-system/wiki).
+The __samples__ can be found [here](https://github.com/yousef-essa/packet-system-sample).  
+The __documentation__ can be found [here](https://github.com/yousef-essa/packet-system/wiki).  
+The __wrapper of__ both _client_ & _server_ can be found [here](https://github.com/yousef-essa/packet-system-default).
 
 Install with NPM:
 > npm install packet-system
--- src/PacketUtil.ts
+++ src/PacketUtil.ts
@@ -2,6 +2,8 @@ import Packet from "./Packet";
 import PacketContext from "./PacketContext";
 
 export default class PacketUtil {
+    private static PACKET_TYPE_REGEX: RegExp = /(?<=\[).+?(?=\])/
+
     /**
      * A convenience method for sending a reply message.
      *
@@ -10,5 +12,36 @@ export default class PacketUtil {
      */
     public static reply(context: PacketContext<any>, packet: Packet) {
         context.getPacketHandler().send(packet, context.getFrom())
+    }
+
+    /**
+     * This method attempts to retrieve the packet
+     * type from the given message content.
+     *
+     * @param message the message content to retrieve packet type from.
+     * @return the packet type if applicable, otherwise 'unknown' is returned.
+     */
+    public static packetType(message: string): string {
+        const searchValue = this.PACKET_TYPE_REGEX.exec(message)
+
+        const firstValue = searchValue?.find(((_value, index) => index == 0))
+        if (searchValue == undefined || firstValue == undefined) {
+            return "unknown"
+        }
+
+        return firstValue
+    }
+
+    /**
+     * A convenience method for getting rid of the packet
+     * type from the message content.
+     *
+     * @param type the packet type that will be removed
+     * @param message the message content to remove from
+     * @return the message content without the packet type if applicable,
+     * otherwise the message will return as untouched.
+     */
+    public static removePacketType(type: string, message: string): string {
+        return message.replace(`[${type}] `, '')
     }
 }
